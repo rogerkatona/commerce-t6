ecae535f97920d0398ea712d3f0532065122268e
Roger Katona <roger.katona@gmail.com>
2022-08-18 11:03:33 -0700

    Initial Screen for Frytech

--- components/hero_index.js
+++ components/hero_index.js
@@ -5,7 +5,7 @@ export default function HeroIndex({ children, home }) {
 
  <div className="flex lg:h-screen90vh h-screen90vh bg-cover lg:bg-center bg-center bg-no-repeat flex-col justify-center items-center mt-10">
      <div className="text-9xl text-white.100">FRYTECH</div>
-     <div className="text-2xl text-gray.400">coming soon...</div>
+     <div className="text-2xl text-gray.400">coming soon now...</div>
      {/*                     <div className="flex flex-row lg:mx-auto lg:max-w-7xl md:p-12 p-6">
                     <div className="font-bebasNeue md:text-6xl text-4xl text-center text-white.100 opacity-80 lg:w-2/3 w-1/2 ">
                     Helping UX Teams Thrive in Today's Competitive Landscape with professional UX services and workshops.
